59d752c1aa586a2a276799957a7100abd48881df
araujo.wg <araujo.wg@gmail.com>
2017-03-30 12:34:34 -0300

    Initianlizing project!!!!

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './index.css';
+
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');
+
+// debugger;
+if (courseValue > 0) {
+    console.log('I would pay ${courseValue} for this course!'); // eslint-disable-line no-console
+}
